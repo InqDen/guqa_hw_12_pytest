39e76a13983fede8b05de7898c5e4e7765c39b81
InqDen <inq.den@yande.ru>
2022-09-07 19:58:31 +0500

    delete

--- tests/test_git.py
+++
@@ -1,8 +0,0 @@
-from selene import browser, have
-
-
-def test_open_and_search():
-    browser.open('https://github.com')
-    browser.element('.btn-mktg').click()
-    #browser.element('.sr-only').should(have.exact_text('Welcome to GitHub!'))
-    browser.element('h1').should(have.exact_text('Sign in to GitHub'))
